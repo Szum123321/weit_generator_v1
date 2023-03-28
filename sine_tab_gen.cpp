92b2f68dcd06f02d0c4b219fbd4605b79083aa5f
Szum123321 <szymek.januszek@gmail.com>
2023-03-28 21:24:08 +0200

    init

---
+++ sine_tab_gen.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1024, A = 127;
+
+int main() {
+	for(int i = 0; i < N; i++) {
+		int x = A + roundl(A * sinl((2 * M_PI * i) / (long double)N));
+		cout << "0x" << uppercase << hex  << x << ", ";
+	}
+}
